5771099231140d2fb2e5f91fadca6849c745dba8
aroanjali <aroanjali@users.noreply.github.com>
2016-07-04 05:50:09 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,11 +1,17 @@
 library("shiny")
-
+library(markdown)
 # Function to predict Demand based on price. Demand has an inverse relationship with Price
-demandPredict <- function(price) 2000/price
+demandPredict <- function(price) {
+  ifelse(price<=0,NA,
+  ifelse((price>100),0,200 -(2*price))
+  )
+  }
 
 shinyServer(
   function(input,output){
     output$inputValue <- renderPrint({input$price})
     output$prediction <- renderPrint({demandPredict(input$price)})
   }
+  
+
 )
--- ui.R
+++ ui.R
@@ -1,17 +1,70 @@
 #install.packages("shiny")
+#setwd("C:/Users/aarora/Personal/Anjali/Personal/Coursera - Data Science/Course 8 - Developing Data Products/DevelopingDataProducts")
 library("shiny")
+library(markdown)
+# mainPanel(
+#   #              includeMarkdown("about.md")
+#   #            )
 
-shinyUI(pageWithSidebar(
-  headerPanel("Demand Prediction - To predict the demand for different price points of a given product"),
-  sidebarPanel(
-   numericInput('price', 'Price $', 100, min=50, max=200, step =5),
-   submitButton('Submit')
+
+
+
+# 
+#  shinyUI(
+#    pageWithSidebar(
+#    headerPanel("Demand Prediction - To predict the demand for different price points of a given product"),
+#    sidebarPanel(
+#     numericInput('price', 'Price $', 50, min=0, max=200, step =5),
+#     submitButton('Submit')
+#    ),
+#    mainPanel(
+#      h3('Results of prediction'),
+#      h4('You entered Price($) of '),
+#      verbatimTextOutput("inputValue"),
+#      h4('Which resulted in a prediction of Demand(units) of'),
+#      verbatimTextOutput("prediction")
+#    )
+#  )
+#  )
+ 
+ 
+ 
+ 
+ shinyUI(
+   navbarPage("Demand Prediction",
+      tabPanel("Application",        
+     headerPanel("Demand Prediction - To predict the demand for different price points of a given product"),
+     sidebarPanel(
+       numericInput('price', 'Price $', 50, min=0, max=200, step =5),
+       submitButton('Submit')
+     ),
+     
+     mainPanel(
+       # tabsetPanel(
+       #   tabPanel("Demand Prediction Application",verbatimTextOutput("inputValue")),
+       h3('Results of prediction'),
+       h4('You entered Price($) of '),
+       verbatimTextOutput("inputValue"),
+       h4('Which resulted in a prediction of Demand(units) of'),
+       verbatimTextOutput("prediction"))
+     
+      
+    # tabPanel("About", includeMarkdown("about.md") )  
+    # )
+    # )
+     # ,
+    #tabPanel("About",includeMarkdown("about.rmd"))
+    # )
+     
+     
+     
+     
+   
   ),
-  mainPanel(
-    h3('Results of prediction'),
-    h4('You entered Price($) of '),
-    verbatimTextOutput("inputValue"),
-    h4('Which resulted in a prediction of Demand(units) of'),
-    verbatimTextOutput("prediction")
-  )
-))
+  tabPanel("About",includeMarkdown("about.rmd"))
+ )
+ )
+ 
+ 
+ 
+ 
